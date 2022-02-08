56e83f7c661d1490074ecf922587daeea5cf9c37
Italo PInheiro <italo_00pinheiro@hotmail.com>
2022-02-08 01:33:57 -0300

    comiit

--- config/db.js
+++ config/db.js
@@ -1,3 +1,6 @@
+// para conexão com o bd
+// código para diferenciar o link de produção ou de localhost
+
 if(process.env.NODE_ENV == "production"){
     module.exports = {mongoURI: "mongodb+srv://italopinheiro:pinheiro97@blog-app.jb5ys.mongodb.net/test"}
 }else{
